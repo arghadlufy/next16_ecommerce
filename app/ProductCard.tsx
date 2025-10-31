e1fdfe03d662dba08fae7bff0636d5f8854e09e9
arghadlufy <arghadlufy@gmail.com>
2025-10-31 18:10:20 +0530

    fetch products from database in Server component

--- app/ProductCard.tsx
+++ app/ProductCard.tsx
@@ -1,19 +1,21 @@
-import { Product } from "@/lib/mocks";
 import { formatPrice } from "@/lib/utils";
 import Image from "next/image";
+import { Product } from "./generated/prisma/client";
 
 export default function ProductCard({ product }: { product: Product }) {
   return (
     <div className="border rounded-lg shadow-md p-4">
       <div className="relative aspect-video">
         {/* Automatically lazy load */}
-        <Image
-          src={product.image}
-          alt={product.name}
-          className="object-cover"
-          fill
-          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-        />
+        {product.image && (
+          <Image
+            src={product.image}
+            alt={product.name}
+            className="object-cover"
+            fill
+            sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+          />
+        )}
         {/* for lg screen, as multi column so images can be lower in quality as user cannot notice that, so 33vw quality, for mobile screen its 1 column layout so 100vw quality as Images will be more visible and should not look bad */}
       </div>
       <h3 className="text-lg font-bold mb-2">{product.name}</h3>
